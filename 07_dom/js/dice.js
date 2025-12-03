597197b1515ed5fb82b95324e53ad1a9cfda69a8
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-12-03 15:42:45 +0900

    update

--- 07_dom/js/dice.js
+++ 07_dom/js/dice.js
@@ -141,7 +141,7 @@ createDiceImages();
 diceContainer.innerHTML= '<span class="text-7xl">1</span>'
 
 // TODO: 1のサイコロ画像を表示: innerHTML: diceImages[1] を代入
-
+diceContainer.innerHTML = diceImages[1]
 
 // ボタンクリックイベント
 btnRoll.addEventListener('click', rollDice);
