83467ed4767ab0dafb84fc054f5a64a40991bf69
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-02-18 15:03:04 +0900

    update

--- 11_event/basic/js/app.js
+++ 11_event/basic/js/app.js
@@ -20,6 +20,9 @@ const inputText = document.getElementById('input-text');
 const inputMsg = document.querySelector('#input-msg span');
 
 // TODO: inputイベントの処理を実装: addEventListener('input', function(event) {
+inputText.addEventListener('input', (event) => {
+    inputMsg.textContent = event.target.value
+})
 
 // 3. Mouseover/Mouseoutイベント
 // マウスが要素に乗った時、離れた時に色を変える
